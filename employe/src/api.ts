ef0a377d9f6ba80bdbc8da2ffff887f7e0c08a32
Mathilde01m <91540529+Mathilde01m@users.noreply.github.com>
2024-11-05 15:07:33 +0100

    update connection system to employe

--- employe/src/api.ts
+++ employe/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Configure l'URL de l'API Strapi
-const API_URL = 'http://192.168.1.4:1337/api'; // Remplace par l'URL de ton backend Strapi si nécessaire
+const API_URL = 'http://10.76.203.111:1337/api'; // Remplace par l'URL de ton backend Strapi si nécessaire
 
 // Crée une instance Axios avec des paramètres par défaut
 const api = axios.create({
@@ -12,14 +12,30 @@ const api = axios.create({
 });
 
 // Fonction pour l'inscription
-export const registerUser = async (userData: { username: string; email: string; password: string; role: string }) => {
+export const registerUser = async (userData) => {
   return await api.post('/users', userData);
 };
 
 // Fonction pour la connexion
-export const loginUser = async (userData: { identifier: string; password: string }) => {
-    // Utilise le bon point de terminaison pour l'authentification
-    return await api.post('/auth/local', userData);
-  };
+export const loginUser = async (userData) => {
+  // Utilise le bon point de terminaison pour l'authentification
+  return await api.post('/auth/local', userData);
+};
+
+// Fonction pour récupérer les détails de l'utilisateur avec son ID et le JWT
+export const getUserDetails = async (token) => {
+  try {
+    const response = await api.get(`/users/me?populate=role`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des détails de l'utilisateur :", error.response?.data || error.message);
+    throw error;
+  }
+};
+
 
 export default api;
